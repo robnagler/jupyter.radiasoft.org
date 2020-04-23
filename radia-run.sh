1199cf84ef8efb070c052528f1a0204aa1f060c7
robnagler <github@q33.us>
2020-04-23 16:36:39 +0000

    empty command

--- radia-run.sh
+++ radia-run.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-echo $(date) robnagler here ${PYENV_VERSION:-not set}
-if [[ ! -r ~/.jupyter/jupyter_notebook_config.json ]]; then
-    jupyter serverextension enable --py jupyterlab --user
-fi
+: nothing for now
