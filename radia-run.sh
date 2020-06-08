9db824afd03e3074e30c6ed5fd3f561f4299f9ac
robnagler <github@q33.us>
2020-06-08 22:41:52 +0000

    only run on alpha

--- radia-run.sh
+++ radia-run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 : nothing for now
-(
-    echo nothing
-    exit
-    set -e
-    install_source_bashrc
-    gcl radiasoft/rsopt
-    cd rsopt
-    install_not_strict_cmd pyenv shell py3
-    pip install .
-) >& ~/.tmp-jupyter.radiasoft.org.err
+if [[ ${JUPYTERHUB_ACTIVITY_URL:-} =~ 10.1.2.6 ]]; then
+    (
+        set -e
+        install_source_bashrc
+        gcl radiasoft/rsopt
+        cd rsopt
+        install_not_strict_cmd pyenv shell py3
+        pip install .
+    ) >& ~/.tmp-jupyter.radiasoft.org.err
+fi
