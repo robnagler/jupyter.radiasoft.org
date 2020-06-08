61341962a4ea3c15b7c7638ae3feda9e1653684d
robnagler <github@q33.us>
2020-06-08 23:17:45 +0000

    moved to global

--- radia-run.sh
+++ radia-run.sh
@@ -1,12 +1,2 @@
 #!/bin/bash
 : nothing for now
-if [[ ${JUPYTERHUB_ACTIVITY_URL:-} =~ 10.1.2.[56] ]]; then
-    (
-        set -e
-        install_source_bashrc
-        gcl radiasoft/rsopt
-        cd rsopt
-        install_not_strict_cmd pyenv shell py3
-        pip install .
-    ) >& ~/.tmp-jupyter.radiasoft.org.err
-fi
