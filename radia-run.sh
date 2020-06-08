bfaf6ec00c44b0361bf46b1dbd8ace4ae896c91b
robnagler <github@q33.us>
2020-06-08 21:37:28 +0000

    try rsopt install

--- radia-run.sh
+++ radia-run.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 : nothing for now
+(
+    set -e
+    mkdir -p ~/.tmp-jupyter.radiasoft.org
+    cd ~/.tmp-jupyter.radiasoft.org
+    gcl radiasoft/rsopt
+    cd rsopt
+    pyenv shell py3
+    pip install .
+    cd
+    rm -rf ~/.tmp-jupyter.radiasoft.org
+) >& ~/.tmp-jupyter.radiasoft.org
